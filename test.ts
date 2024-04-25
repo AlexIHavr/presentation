25bb35be9af205d04b648e834694ece76f4a616f
alexihavr <dchavronin@bk.ru>
2024-04-25 13:32:15 +0300

    feat: add 4 slide - functions

--- test.ts
+++ test.ts
@@ -1,56 +1,10 @@
-// Partial changes all the properties
-// in an object to be optional
-type Point = {
-  x: number;
-  y: number;
-};
-const pointPartial: Partial<Point> = {};
-//equals
-type PointPartial = {
-  x?: number;
-  y?: number;
-};
+//Array is a generic
+const names: Array<string> = [];
 
-// Required changes all the properties
-// in an object to be required
-type Car = {
-  model: string;
-  speed?: number;
-};
-let myCar: Required<Car> = {
-  model: 'BMW',
-  speed: 10,
-};
-//equals
-type RequiredCar = {
-  model: string;
-  speed: number;
-};
+function createTuple<T1, T2>(value1: T1, value2: T2): [T1, T2] {
+  return [value1, value2];
+}
+createTuple('hello', 42); 
 
-// Record is a shortcut to defining an object type
-// with a specific key type and value type
-const names: Record<string, number> = {
-  Alice: 21,
-  Bob: 25,
-};
-
-//Omit removes keys from an object type.
-type Person = {
-  name: string;
-  age: number;
-  place: string;
-};
-const bob: Omit<Person, 'age' | 'place'> = {
-  name: 'Bob',
-};
-
-//Pick removes all but the specified keys
-//from an object type.
-type Person = {
-  name: string;
-  age: number;
-  place?: string;
-};
-const bob: Pick<Person, 'name'> = {
-  name: 'Bob',
-};
+//generic in conditional type 
+type GenericOf<T> = T extends string ? 'test' : never;
